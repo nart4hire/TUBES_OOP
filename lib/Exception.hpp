b2cee536c4f6f2efea3865863cec806ee3b9d2f3
nart4hire <nart4hire@users.noreply.github.com>
2022-03-23 19:45:15 +0700

    Created Exception System
    Added Exception class and Exceptions pertaining to Inventory

---
+++ lib/Exception.hpp
@@ -0,0 +1,66 @@
+#ifndef EXCEPTION_HEADER
+#define EXCEPTION_HEADER
+
+#include <string>
+
+namespace mobicraft
+{
+    class Exception
+    {
+    protected:
+        const int id;
+    public:
+        Exception(int id) : id(id) {}
+        ~Exception() {}
+
+        int getId() { return this->id; }
+        virtual std::string what() = 0;
+    };
+
+    class NothingSlotException : public Exception
+    {
+    public:
+        NothingSlotException() : Exception(1) {}
+        ~NothingSlotException() {}
+
+        std::string what() { return "Nothing was in source slot."; }
+    };
+
+    class CrashSlotException : public Exception
+    {
+    public:
+        CrashSlotException() : Exception(2) {}
+        ~CrashSlotException() {}
+
+        std::string what() { return "Something different already in destination slot."; }
+    };
+
+    class NumberTooBigException : public Exception
+    {
+    public:
+        NumberTooBigException() : Exception(3) {}
+        ~NumberTooBigException() {}
+
+        std::string what() { return "Number inputed was too big."; }
+    };
+
+    class NotExistsException : public Exception
+    {
+    public:
+        NotExistsException() : Exception(4) {}
+        ~NotExistsException() {}
+
+        std::string what() { return "Input does not exist."; }
+    };
+
+    class WrongTypeException : public Exception
+    {
+    public:
+        WrongTypeException() : Exception(5) {}
+        ~WrongTypeException() {}
+
+        std::string what() { return "Input of the wrong type."; }
+    };
+}
+
+#endif
